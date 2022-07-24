9f42c455c35efb838c395f5c7ee52d604456b1fc
Silver-Falcon <Silver-Falcon@users.noreply.github.com>
2022-07-24 16:12:42 +0300

    Added attack type

--- src/pokemon/attack.py
+++ src/pokemon/attack.py
@@ -1,18 +1,23 @@
 class Attack:
     name = ""
+    _type = ""
     damage = 0
 
 
-    def __init__(self, name = "שם המתקפה", damage = "כמה נזק היא מורידה"):
+    def __init__(self, name = "שם המתקפה", damage = "כמה נזק היא מורידה", _type = "סוג"):
         self.name = name
         self.damage = damage
+        self._type = _type
 
     def __str__(self):
         return str(vars(self))
-
+    
+    def __repr__(self):
+        return str(self)
+    
     def get_damage(self, pokemon):
         result = self.damage
-        if (pokemon["type"] == self.name):
+        if (pokemon["type"] == self._type):
             result = result-5
         return result
         
